d46b68b1509a2effaf7ee73e5f0fb48634abf9c4
InedoJohn <jrasch@inedo.com>
2018-02-15 12:06:02 -0500

    Reorg

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.BuildMaster.Extensibility;
+
+[assembly: AssemblyTitle("FTPLegacy")]
+[assembly: AssemblyDescription("Contains legacy actions for communicating with a server using FTP. The FTP Extension is also required.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: RequiredBuildMasterVersion("6.0.0")]
